6d8ec5d5f629955677f5e09fe666627c25a0c15b
Alexander Eversbusch <alexandereversbusch@AlexandersMBP2.attlocal.net>
2022-06-17 02:46:33 -0400

    Try getting navlink active styling working. WIP.

--- src/components/NavLink.svelte
+++ src/components/NavLink.svelte
@@ -1,9 +1,11 @@
 <script>
+  import { page } from '$app/stores';
   export let title;
   export let href;
 </script>
 
-<a {href} class="link bg-gray-900 text-white px-3 py-2 rounded-md text-sm font-medium" aria-current="page">
+<a {href} class="link bg-gray-900 text-white px-3 py-2 rounded-md text-sm font-medium" aria-current="page"
+  class:active="{$page.url.pathname.includes(href)}">
   {title}
 </a>
 
